2bc75f0fe8e64ec191b9ef22224de28ee556a144
xinglong-zhang <xinglong.zhang@cuhk.edu.hk>
2026-09-15 14:40:04 +0800

    deleted epub options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,9 +30,5 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
-
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
